521529550a033709ab7e98b7251287875f05fd9a
Rudraksh Kumrawat <83749855+rudrakshk-101@users.noreply.github.com>
2023-08-30 14:54:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var $loginMsg = $('.loginMsg'),
+  $login = $('.login'),
+  $signupMsg = $('.signupMsg'),
+  $signup = $('.signup'),
+  $frontbox = $('.frontbox'),
+  $bod = $('body');
+
+$('#switch1').on('click', function() {
+  $loginMsg.toggleClass("visibility");
+  $frontbox.addClass("moving");
+  $bod.css('background-image', 'url("/office-supplies.jpg")');
+  $signupMsg.toggleClass("visibility");
+  $signup.toggleClass('hide');
+  $login.toggleClass('hide');
+})
+
+$('#switch2').on('click', function() {
+    $loginMsg.toggleClass("visibility");
+    $frontbox.removeClass("moving");
+    $signupMsg.toggleClass("visibility");
+    $bod.css('background-image', 'url("/workplace-with-blue-office-supplies.jpg")');
+  $signup.toggleClass('hide');
+  $login.toggleClass('hide');
+})
+
+setTimeout(function(){
+  $('#switch1').click()
+},1000)
+
+setTimeout(function(){
+  $('#switch2').click()
+},3000)
